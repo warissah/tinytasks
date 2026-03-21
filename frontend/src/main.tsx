4e403559834729bd36c23f44486737d9711b4697
warissah <warissahossain@gmail.com>
2026-03-21 14:38:30 -0700

    tailwind for frontend
    added tailwind stuff

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        ADHD Execution Coach
+      </h1>
+    </div>
+  );
+};
+
+export default App;
